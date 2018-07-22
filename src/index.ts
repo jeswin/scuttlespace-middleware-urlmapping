635230c4a89d02ce0dc0d09883813e8809c44901
Jeswin Kumar <jeswinpk@agilehead.com>
2018-07-22 16:45:04 +0530

    WIP

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,20 @@
 import { NextFunction } from "connect";
 import { Context } from "koa";
 
-export default function(domain: string) {
+export default function(opts: {
+  domain: string;
+  graphql: {
+    hostname: string;
+    port: number;
+  };
+}) {
   return async function urlMapper(ctx: Context, next: NextFunction) {
     const fullHostname = ctx.request.header.host.toLowerCase();
     const hostname = fullHostname.startsWith("www.")
       ? fullHostname.substring(4)
       : fullHostname;
 
-    if (hostname === domain.toLowerCase()) {
+    if (hostname === opts.domain.toLowerCase()) {
       const parts = ctx.path.split("/");
       const username = parts.length > 1 ? parts[1] : undefined;
       const account = `
@@ -29,7 +35,4 @@ export default function(domain: string) {
     }
     next();
   };
-
-  console.log("PARTS", parts);
-  console.log(ctx);
 }
---
+++ src/test/test.ts
@@ -0,0 +1,35 @@
+import { NextFunction } from "express-serve-static-core";
+import "mocha";
+import "should";
+import middleware from "../";
+
+const shouldLib = require("should");
+
+function makeContext() {
+  return {
+    path: "/jeswin",
+    request: {
+      header: {
+        host: "localhost"
+      }
+    }
+  };
+}
+
+describe("scuttlespace-middleware-urlmapping", async () => {
+  it("....", async () => {
+    const mapper = middleware({
+      domain: "localhost",
+      graphql: {
+        hostname: "localhost",
+        port: 4000
+      }
+    });
+    const ctx: any = makeContext();
+    const next: NextFunction = () => {};
+    mapper(ctx, next);
+    console.log(ctx);
+
+    shouldLib.exist(ctx.user);
+  });
+});
